7b96b62e1d8a87701468e6f2c2af1f05b5156d5a
rahulkumar-yadav-by <rahulkumar.yadav@blueyonder.com>
2022-11-09 09:26:17 +0530

    Initial Project setup, added dependencies in Kafka_Socket_Chat_App pom.xml

---
+++ src/main/java/com/Spring/Chat/Kafka/Sockets/Application/Kafka_Socket_Chat_App/KafkaSocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.Chat.Kafka.Sockets.Application.Kafka_Socket_Chat_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSocketChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Spring/Chat/Kafka/Sockets/Application/Kafka_Socket_Chat_App/KafkaSocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring.Chat.Kafka.Sockets.Application.Kafka_Socket_Chat_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
